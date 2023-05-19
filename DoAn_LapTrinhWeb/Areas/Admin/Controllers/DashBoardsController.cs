54a918947f209c754aea93ace20ae803537b3118
AnhVietBui <vbui01050@gmail.com>
2023-05-19 17:18:27 +0700

    Thêm thống kê doanh thu có lọc theo ngày

--- DoAn_LapTrinhWeb/Areas/Admin/Controllers/DashBoardsController.cs
+++ DoAn_LapTrinhWeb/Areas/Admin/Controllers/DashBoardsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoAn_LapTrinhWeb.Model;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -19,44 +20,52 @@ namespace DoAn_LapTrinhWeb.Areas.Admin.Controllers
             ViewBag.ListOrderDetail2 = db.Oder_Detail.Where(m => m.Product.type == 2).OrderByDescending(m => m.create_at).Take(3).ToList();
             ViewBag.ListOrder = db.Orders.OrderByDescending(m => m.oder_date).Take(7).ToList();
             return View();
+
         }
 
         //THÊM THỐNG KÊ
-        //[HttpGet]
-        //public ActionResult GetStatistical(string fromDate, string toDate)
-        //{
-        //    var query = from o in db.Orders
-        //                join od in db.Oder_Detail
-        //                on o.order_id equals od.order_id
-        //                join p in db.Products
-        //                on od.product_id equals p.product_id
-        //                select new
-        //                {
-        //                    CreatedDate = o.create_at,
-        //                    Quantity = od.quantity,
-        //                    Price = od.price
-        //                };
-        //    if (!string.IsNullOrEmpty(fromDate))
-        //    {
-        //        DateTime startDate = DateTime.ParseExact(fromDate, "dd/MM/yyyy", null);
-        //        query = query.Where(x => x.CreatedDate >= startDate);
-        //    }
-        //    if (!string.IsNullOrEmpty(toDate))
-        //    {
-        //        DateTime endDate = DateTime.ParseExact(toDate, "dd/MM/yyyy", null);
-        //        query = query.Where(x => x.CreatedDate < endDate);
-        //    }
+        [HttpGet]
+        public ActionResult GetStatistical(string fromDate, string toDate)
+        {
+            DateTime? startDate = null;
+            DateTime? endDate = null;
 
-        //    var result = query.GroupBy(x => DbFunctions.TruncateTime(x.CreatedDate)).Select(x => new
-        //    {
-        //        Date = x.Key.Value,                
-        //        TotalSell = x.Sum(y => y.Quantity * y.Price),
-        //    }).Select(x => new
-        //    {
-        //        Date = x.Date,
-        //        DoanhThu = x.TotalSell                
-        //    });
-        //    return Json(new { Data = result }, JsonRequestBehavior.AllowGet);
-        //}
+            if (!string.IsNullOrEmpty(fromDate))
+            {
+                startDate = DateTime.ParseExact(fromDate, "dd/MM/yyyy", null);
+            }
+
+            if (!string.IsNullOrEmpty(toDate))
+            {
+                endDate = DateTime.ParseExact(toDate, "dd/MM/yyyy", null);
+            }
+
+            var query = from o in db.Orders
+                        join od in db.Oder_Detail on o.order_id equals od.order_id
+                        join p in db.Products on od.product_id equals p.product_id
+                        where (startDate == null || o.create_at >= startDate)
+                              && (endDate == null || o.create_at <= endDate)
+                        select new
+                        {
+                            CreatedDate = o.create_at,
+                            Quantity = od.quantity,
+                            Price = od.price
+                        };
+
+            var result = query.GroupBy(x => DbFunctions.TruncateTime(x.CreatedDate))
+                              .Select(x => new
+                              {
+                                  Date = x.Key.Value,
+                                  TotalSell = x.Sum(y => y.Quantity * y.Price),
+                              })
+                              .Select(x => new
+                              {
+                                  Date = x.Date,
+                                  DoanhThu = x.TotalSell
+                              });
+
+            return Json(new { Data = result }, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
